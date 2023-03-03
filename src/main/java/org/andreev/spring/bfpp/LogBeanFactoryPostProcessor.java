5769a49efc4dd53b536a00f4db337f315a7aa182
Bato0n4ik <bato0n4ik.47@gmail.com>
2023-03-04 01:05:02 +0300

    custom post processor spring

---
+++ src/main/java/org/andreev/spring/bfpp/LogBeanFactoryPostProcessor.java
@@ -0,0 +1,22 @@
+package org.andreev.spring.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.PriorityOrdered;
+
+public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        for(String beanDefNames: beanFactory.getBeanDefinitionNames()){
+            var beanDefinition = beanFactory.getBeanDefinition(beanDefNames);
+            beanDefinition.getConstructorArgumentValues().getIndexedArgumentValues().values()
+                    .stream().forEach(x->x.getValue().toString());
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return PriorityOrdered.HIGHEST_PRECEDENCE;
+    }
+}
---
+++ src/main/java/org/andreev/spring/bfpp/VerifyPropertyBeanFactoryPostProcessor.java
@@ -0,0 +1,20 @@
+package org.andreev.spring.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.PriorityOrdered;
+
+public class VerifyPropertyBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println();
+    }
+
+    @Override
+    public int getOrder() {
+        return PriorityOrdered.LOWEST_PRECEDENCE;
+    }
+}
