f6f8a98a7e932c761ccb37ff4bbe0cbcf8673cde
Bato0n4ik <bato0n4ik.47@gmail.com>
2023-12-28 03:44:33 +0300

    add web-controller and JSP pages, add two get responses, mapped JSP on web-controller

---
+++ src/main/java/com/dmdev/spring/http/controller/GreetingController.java
@@ -0,0 +1,26 @@
+package com.dmdev.spring.http.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/api/v1")
+public class GreetingController {
+
+
+    @GetMapping("/hello")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request){
+        modelAndView.setViewName("greeting/hello");
+        return modelAndView;
+    }
+
+    @GetMapping("/bye")
+    public ModelAndView bye(ModelAndView modelAndView){
+        modelAndView.setViewName("greeting/bye");
+        return modelAndView;
+    }
+}
