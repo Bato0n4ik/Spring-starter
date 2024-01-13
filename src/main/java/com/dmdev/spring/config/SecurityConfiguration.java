1c1413dc2b5e9502e5d095e6ab95b47a43b8cd24
Bato0n4ik <bato0n4ik.47@gmail.com>
2024-01-13 21:46:10 +0300

    Authentication functionality via the Google Cloud server has been implemented! The problem of mismatch between user data types and the accepted type of the remote authentication server has been resolved!

--- src/main/java/com/dmdev/spring/config/SecurityConfiguration.java
+++ src/main/java/com/dmdev/spring/config/SecurityConfiguration.java
@@ -1,18 +1,30 @@
 package com.dmdev.spring.config;
 
 import com.dmdev.spring.database.entity.Role;
-import org.springframework.context.annotation.Bean;
+import com.dmdev.spring.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.proxy.Proxy;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+
+import java.lang.reflect.Method;
+import java.util.Set;
 
 @Configuration
-@EnableMethodSecurity // для работы аннтаций @PreAuthorize, @PostAuthorize, @PreFilter, @PostFilter
+@EnableMethodSecurity// для работы аннтаций @PreAuthorize, @PostAuthorize, @PreFilter, @PostFilter
+@RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    private final UserService userService;
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -32,12 +44,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         .deleteCookies("JSESSIONID"))
                 .formLogin(login -> login
                         .loginPage("/login")
+                        .defaultSuccessUrl("/users"))
+                .oauth2Login(config -> config
+                        .loginPage("/login")
                         .defaultSuccessUrl("/users")
-                );
+                        .userInfoEndpoint(userInfo -> userInfo.oidcUserService(oidcUserService())));
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService(){
+        return userRequest -> {
+
+            String email = userRequest.getIdToken().getEmail();
+            // TODO: create user userService.create
+            var userDetails = userService.loadUserByUsername(email);
+
+            var oidcUser = new DefaultOidcUser(userDetails.getAuthorities(), userRequest.getIdToken());
+
+            Set<Method> methodDetailsMethods = Set.of(UserDetails.class.getMethods());
+            return (OidcUser) Proxy.newProxyInstance(SecurityConfiguration.class.getClassLoader(),
+                    new Class[]{OidcUserRequest.class, OidcUser.class}
+                    , (proxy, method, args) -> methodDetailsMethods.contains(method)
+                            ? method.invoke(userDetails, args) : method.invoke(oidcUser, args));
+        };
     }
+
 }
